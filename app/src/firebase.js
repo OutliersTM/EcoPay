e2e108d8ce1d7616c72ace9e90eee62419c4ea72
amn-max <ayushmnaik@gmail.com>
2022-02-06 15:12:49 +0530

    Web App

---
+++ app/src/firebase.js
@@ -0,0 +1,44 @@
+import * as firebase from "firebase/app";
+import "firebase/messaging";
+import "firebase/auth";
+import "firebase/functions";
+const config = {
+  apiKey: "AIzaSyBXagrD84VXx8OyHe7p1qrBfu_y5v1cql8",
+  authDomain: "ecopaymlr.firebaseapp.com",
+  projectId: "ecopaymlr",
+  storageBucket: "ecopaymlr.appspot.com",
+  messagingSenderId: "362519129536",
+  appId: "1:362519129536:web:e085b88dbca315ff7aac25",
+  measurementId: "G-LKVWK42PLC",
+};
+firebase.initializeApp(config);
+
+if ("Notification" in window) {
+  const messaging = firebase.messaging();
+  messaging.usePublicVapidKey(process.env.REACT_APP_FIREBASE_MESSAGING_CERT);
+
+  messaging.onMessage((payload) => {
+    console.log("Message received. ", payload);
+    // push message to UI
+  });
+
+  messaging.onTokenRefresh(() => {
+    const db = firebase.firestore();
+    messaging
+      .getToken()
+      .then((refreshedToken) => {
+        db.collection("users")
+          .doc(firebase.auth().currentUser.uid)
+          .update({ pushTokenWeb: refreshedToken })
+          .then(() => {
+            console.log("Token updated.");
+          })
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => {
+        console.log("Unable to retrieve refreshed token ", err);
+      });
+  });
+}
+
+export default firebase;
